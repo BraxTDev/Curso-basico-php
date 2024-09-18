4c36c35740d138b26a1e38dca1d17190a1941a52
sergio <braxdev48@gmail.com>
2024-09-17 19:41:14 -0500

    curso-php modificando

--- programa/hora.php
+++ programa/hora.php
@@ -1,17 +1,21 @@
 <?php
 //me ayudar pedirle a un usuario los datos
-$segundos = readline("Ingresa el tiempo en segundos: ");
+ $segundos = readline("Ingresa el tiempo en segundos: ");
+ $horas = $segundos / 3600;
+ $segundos = $segundos % 3600;
+ $minutos = $segundos / 60;
+ $segundos = $segundos % 60;
 
-$horas = $segundos / 3600;
-$segundos = $segundos % 3600;
-$minutos = $segundos / 60;
-$segundos = $segundos % 60;
 
-
-echo $horas . " " . $segundos;
+ echo $horas . " " . $segundos;
 
 
 
 echo $segundos;
+
+$los_michis_duermen = true;
+$los_michis_vuelan = false;
+
+var_dump($los_michis_duermen || $los_michis_vuelan);
 
 
